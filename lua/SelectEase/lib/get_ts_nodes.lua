80e1d3ebac06facae283c0c465aed18de2ef1ba3
Nick Hoffman <njhoffman1982@gmail.com>
2023-04-11 13:45:10 -0500

    fix deprecated function

--- lua/SelectEase/lib/get_ts_nodes.lua
+++ lua/SelectEase/lib/get_ts_nodes.lua
@@ -2,67 +2,67 @@ local M = {}
 
 -- https://github.com/nvim-treesitter/nvim-treesitter/blob/master/lua/nvim-treesitter/parsers.lua
 local filetype_to_parsername = {
-    javascriptreact = "javascript",
-    ecma = "javascript",
-    jsx = "javascript",
-    PKGBUILD = "bash",
-    html_tags = "html",
-    typescriptreact = "tsx",
-    ["html.handlebars"] = "glimmer",
-    systemverilog = "verilog",
-    cls = "latex",
-    sty = "latex",
-    OpenFOAM = "foam",
-    pandoc = "markdown",
-    rmd = "markdown",
-    quarto = "markdown",
-    cs = "c_sharp",
-    tape = "vhs",
-    dosini = "ini",
-    confini = "ini",
+  javascriptreact = "javascript",
+  ecma = "javascript",
+  jsx = "javascript",
+  PKGBUILD = "bash",
+  html_tags = "html",
+  typescriptreact = "tsx",
+  ["html.handlebars"] = "glimmer",
+  systemverilog = "verilog",
+  cls = "latex",
+  sty = "latex",
+  OpenFOAM = "foam",
+  pandoc = "markdown",
+  rmd = "markdown",
+  quarto = "markdown",
+  cs = "c_sharp",
+  tape = "vhs",
+  dosini = "ini",
+  confini = "ini",
 }
 
 local get_parser_name_and_root = function()
-    local lang = vim.bo[0].ft
-    local parser_name = filetype_to_parsername[lang] or lang
+  local lang = vim.bo[0].ft
+  local parser_name = filetype_to_parsername[lang] or lang
 
-    local parser_ok, parser = pcall(vim.treesitter.get_parser, 0, parser_name)
+  local parser_ok, parser = pcall(vim.treesitter.get_parser, 0, parser_name)
 
-    if parser_ok then
-        local trees = parser:parse()
-        local root = trees[1]:root()
+  if parser_ok then
+    local trees = parser:parse()
+    local root = trees[1]:root()
 
-        return parser_name, root
-    end
+    return parser_name, root
+  end
 end
 
 M.get_nodes_from_query = function(query, queries)
-    local parser_name, root = get_parser_name_and_root()
-    local nodes = {}
+  local parser_name, root = get_parser_name_and_root()
+  local nodes = {}
 
-    query = queries[parser_name] or query
-    if query == nil then
-        return {}
-    end
+  query = queries[parser_name] or query
+  if query == nil then
+    return {}
+  end
 
-    local iter_query = vim.treesitter.query.parse_query(parser_name, query)
-    for _, matches, _ in iter_query:iter_matches(root) do
-        local node = matches[1]
-        table.insert(nodes, node)
-    end
+  local iter_query = vim.treesitter.query.parse(parser_name, query)
+  for _, matches, _ in iter_query:iter_matches(root) do
+    local node = matches[1]
+    table.insert(nodes, node)
+  end
 
-    return nodes
+  return nodes
 end
 
 M.get_text_from_nodes = function(nodes)
-    local text_tbl = {}
+  local text_tbl = {}
 
-    for _, node in ipairs(nodes) do
-        local node_text = vim.treesitter.get_node_text(node, 0)
-        table.insert(text_tbl, node_text)
-    end
+  for _, node in ipairs(nodes) do
+    local node_text = vim.treesitter.get_node_text(node, 0)
+    table.insert(text_tbl, node_text)
+  end
 
-    return text_tbl
+  return text_tbl
 end
 
 return M
